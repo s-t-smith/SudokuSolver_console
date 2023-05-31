1d4ac4525bc3954f53ac19520edd5fbcfd8f5418
Sean Smith <sean.smith01@l3harris.com>
2023-05-31 13:48:35 -0500

    Re-added block class.

--- SudokuSolver/SudokuSolver/SudokuBlock.h
+++ SudokuSolver/SudokuSolver/SudokuBlock.h
@@ -9,81 +9,43 @@
 class SudokuBlock
 {
 /* 
-* This class will be a simple collection of lower-level objects.
-* The functions of those objects will be explosed through this layer's public functions:
-* SudokuBlock - Default constructor. No explicit constructor is implemented for this project, which would require some tricky argument passing from the higher layers.
-* ~SudokuBlock - Default destructor; clears out the memory used by the block.
-* setBlockCellVal - Dereferences an array element and writes a number to that cell.
-* getBlockCellVal - Dereferences an array element and returns the number written in that cell.
-* setBlockCellNote - Dereferences an array element and sets the boolean in that cell's notes list as 'true'.
-* clearBlockCellNote - Dereferences an array element and sets the boolean in that cell's notes list as 'false'.
-* getBlockCellNote - Dereferences an array element and returns the boolean value at that cell's notes list index.
-* blockValPresent - Checks a 3x3 set for a value, returning whether or not the value appears in any of the cells.
+* TODO: create and document member functions.
 */
 public:
 	SudokuBlock();
+	SudokuBlock(int size);
 	~SudokuBlock();
 	void setBlockCellVal(int row, int col, int val);
 	int getBlockCellVal(int row, int col);
-	void setBlockCellNote(int row, int col, int idx);
-	void clearBlockCellNote(int row, int col, int idx);
+	void setBlockCellNote(int row, int col, int idx, bool set);
 	bool getBlockCellNote(int row, int col, int idx);
 	bool blockValPresent(int val);
+	bool rowValPresent(int val);
+	bool colValPresent(int val);
+	void printBlock();
 
 private:
-	SudokuCell* blockCells[3][3];
+	std::vector<std::vector<SudokuCell*>> blockCells;
 };
 
-SudokuBlock::SudokuBlock()
-{
-	// Creates a 3x3 array of blank cells:
-	for (int r = 0; r < 3; r++) {
-		for (int c = 0; c < 3; c++) {
-			blockCells[r][c] = new SudokuCell();
-		}
-	}
-}
+SudokuBlock::SudokuBlock() {}
 
-SudokuBlock::~SudokuBlock()
-{
-	// Clears the memory from the object:
-	for (int r = 0; r < 3; r++) {
-		for (int c = 0; c < 3; c++) {
-			delete blockCells[r][c];
-		}
-	}
-}
+SudokuBlock::SudokuBlock(int size) {}
 
-void SudokuBlock::setBlockCellVal(int row, int col, int val) {
-	blockCells[row][col]->setVal(val);
-}
+SudokuBlock::~SudokuBlock() {}
 
-int SudokuBlock::getBlockCellVal(int row, int col) {
-	return blockCells[row][col]->getVal();
-}
+void SudokuBlock::setBlockCellVal(int row, int col, int val) {}
 
-void SudokuBlock::setBlockCellNote(int row, int col, int idx) {
-	blockCells[row][col]->setNote(idx);
-}
+int SudokuBlock::getBlockCellVal(int row, int col) {}
 
-void SudokuBlock::clearBlockCellNote(int row, int col, int idx) {
-	blockCells[row][col]->clearNote(idx);
-}
+void SudokuBlock::setBlockCellNote(int row, int col, int idx, bool set) {}
 
-bool SudokuBlock::getBlockCellNote(int row, int col, int idx) {
-	return blockCells[row][col]->getNote(idx);
-}
+bool SudokuBlock::getBlockCellNote(int row, int col, int idx) {}
 
-bool SudokuBlock::blockValPresent(int val) {
-	if (val < 0 || val > 9)
-		throw std::out_of_range("Value not accepted");
-	for (int row = 0; row < 3; row++) {
-		for (int col = 0; col < 3; col++) {
-			if (getBlockCellVal(row, col) == val) {
-				return true;
-			}
-		}
-	}
-	
-	return false;
-}
+bool SudokuBlock::blockValPresent(int val) {}
+
+bool SudokuBlock::rowValPresent(int val) {}
+
+bool SudokuBlock::colValPresent(int val) {}
+
+void SudokuBlock::printBlock() {}
